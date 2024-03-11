85a7baed858ff876a408265c6a9e598beeb743d5
Daman Raaj <damanraajsuryavanshi@yahoo.in>
2024-03-11 21:29:11 +0530

    implement input for add new project

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,25 +1,42 @@
-import React from "react";
+import React, { useRef } from "react";
 import Input from "./ui/Input";
 
-const NewProject = () => {
+const NewProject = ({ onCancel, onSave }) => {
+  const titleRef = useRef();
+  const descriptionRef = useRef();
+  const duedateRef = useRef();
+  const handleOnSave = () => {
+    const newproject = {
+      name: titleRef.current.value,
+      descriptions: descriptionRef.current.value,
+      duedate: duedateRef.current.value,
+    };
+    onSave(newproject);
+  };
   return (
     <div className="w-[35rem] mt-16">
       <menu className="flex items-center justify-end gap-4 my-4">
         <li>
-          <button className="text-stone-800 hover:text-stone-950">
+          <button
+            className="text-stone-800 hover:text-stone-950"
+            onClick={onCancel}
+          >
             Cancel
           </button>
         </li>
         <li>
-          <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">
+          <button
+            onClick={handleOnSave}
+            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+          >
             Save
           </button>
         </li>
       </menu>
       <div>
-        <Input label={"Project Name"} />
-        <Input label={"Due Date"} />
-        <Input label={"Description"} textArea />
+        <Input label={"Project Name"} ref={titleRef} id={"title"} />
+        <Input label={"Due Date"} ref={duedateRef} id={"duedate"} />
+        <Input label={"Description"} ref={descriptionRef} textArea />
       </div>
     </div>
   );
--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { forwardRef } from "react";
 
-const Input = ({ label, textArea, ...props }) => {
+const Input = ({ label, textArea, ...props }, ref) => {
   const classes =
     "w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600";
   return (
@@ -9,12 +9,12 @@ const Input = ({ label, textArea, ...props }) => {
         {label}
       </label>
       {textArea ? (
-        <textArea className={classes} {...props} />
+        <textArea className={classes} {...props} ref={ref} />
       ) : (
-        <input className={classes} {...props} />
+        <input className={classes} {...props} ref={ref} />
       )}
     </p>
   );
 };
 
-export default Input;
+export default forwardRef(Input);
