7d5fe89d580d53fab4a92c5e486bdf478fd46e55
Daman Raaj <damanraajsuryavanshi@yahoo.in>
2024-03-11 20:37:52 +0530

    project sidebar component

---
+++ src/components/ProjectsSideBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Button from "./ui/Button";
+
+const ProjectsSideBar = () => {
+  return (
+    <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-500 md:w-72 rounded-r-xl">
+      <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
+        Your Projects
+      </h2>
+      <div>
+        <Button>+ Add project</Button>
+      </div>
+      <ul></ul>
+    </aside>
+  );
+};
+
+export default ProjectsSideBar;
