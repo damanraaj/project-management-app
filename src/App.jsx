695893f655c25c957a75524bdc6e21a9ae29427b
Daman Raaj <damanraajsuryavanshi@yahoo.in>
2024-03-11 21:28:00 +0530

    update home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,52 @@
+import { useState } from "react";
+import NewProject from "./components/NewProject";
+import NoProjectSelected from "./components/NoProjectSelected";
+import ProjectsSideBar from "./components/ProjectsSideBar";
+
 function App() {
+  const [projects, setProjects] = useState({
+    selected: false,
+    projectList: [],
+  });
+
+  const handleUnselectProject = () => {
+    setProjects((prevState) => {
+      return {
+        ...prevState,
+        selected: false,
+      };
+    });
+  };
+  const handleShowNewProject = () => {
+    setProjects((prevState) => {
+      return {
+        ...prevState,
+        selected: "new",
+      };
+    });
+  };
+
+  const handleAddNewProject = (project) => {
+    setProjects((prevState) => {
+      return {
+        ...prevState,
+        projectList: [...prevState.projectList, project],
+      };
+    });
+  };
   return (
-    <>
-      <h1 className="my-8 text-center text-5xl font-bold">Hello World</h1>
-    </>
+    <main className="h-screen my-8 flex gap-8">
+      <ProjectsSideBar onStartAdd={handleShowNewProject} />
+      {!projects.selected && (
+        <NoProjectSelected onStartAdd={handleShowNewProject} />
+      )}
+      {projects.selected == "new" && (
+        <NewProject
+          onCancel={handleUnselectProject}
+          onSave={handleAddNewProject}
+        />
+      )}
+    </main>
   );
 }
 
