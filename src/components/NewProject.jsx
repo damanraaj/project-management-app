1ae3a6e5a2a7ae45282da55256c71cd9a7fbef5c
Daman Raaj <damanraajsuryavanshi@yahoo.in>
2024-03-13 15:29:09 +0530

    use Modal for invalid input alert

--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,7 +1,9 @@
 import React, { useRef } from "react";
 import Input from "./ui/Input";
+import Modal from "./ui/Modal";
 
 const NewProject = ({ onCancel, onSave }) => {
+  const modalRef = useRef();
   const titleRef = useRef();
   const descriptionRef = useRef();
   const duedateRef = useRef();
@@ -10,7 +12,7 @@ const NewProject = ({ onCancel, onSave }) => {
     const description = descriptionRef.current.value.trim();
     const duedate = duedateRef.current.value.trim();
     if (!title || !description || !duedate) {
-      alert("Please fill all inputs");
+      modalRef.current.open();
       return;
     }
     const newproject = {
@@ -20,33 +22,45 @@ const NewProject = ({ onCancel, onSave }) => {
       duedate,
     };
     onSave(newproject);
+    onCancel();
   };
   return (
-    <div className="w-[35rem] mt-16">
-      <menu className="flex items-center justify-end gap-4 my-4">
-        <li>
-          <button
-            className="text-stone-800 hover:text-stone-950"
-            onClick={onCancel}
-          >
-            Cancel
-          </button>
-        </li>
-        <li>
-          <button
-            onClick={handleOnSave}
-            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
-          >
-            Save
-          </button>
-        </li>
-      </menu>
-      <div>
-        <Input label={"Project Name"} ref={titleRef} id={"title"} />
-        <Input type="date" label={"Due Date"} ref={duedateRef} id={"duedate"} />
-        <Input label={"Description"} ref={descriptionRef} textArea />
+    <>
+      <Modal ref={modalRef} btnCaption={"OK"}>
+        <h2 className="text-xl">Invalid Input</h2>
+        <p className="text-stone-600">Please check your input.</p>
+      </Modal>
+      <div className="w-[35rem] mt-16">
+        <menu className="flex items-center justify-end gap-4 my-4">
+          <li>
+            <button
+              className="text-stone-800 hover:text-stone-950"
+              onClick={onCancel}
+            >
+              Cancel
+            </button>
+          </li>
+          <li>
+            <button
+              onClick={handleOnSave}
+              className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+            >
+              Save
+            </button>
+          </li>
+        </menu>
+        <div>
+          <Input label={"Project Name"} ref={titleRef} id={"title"} />
+          <Input
+            type="date"
+            label={"Due Date"}
+            ref={duedateRef}
+            id={"duedate"}
+          />
+          <Input label={"Description"} ref={descriptionRef} textArea />
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
